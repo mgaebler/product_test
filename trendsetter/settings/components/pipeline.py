56d0728f01c0a5f89a1149b22a7643e4cc246fa6
Marian Gaebler <marian.gaebler@intosite.de>
2014-09-19 06:39:09 +0200

    add bootstrap sass

---
+++ trendsetter/settings/components/pipeline.py
@@ -0,0 +1,34 @@
+# django pipeline default configuration
+## http://django-pipeline.readthedocs.org/en/latest/configuration.html
+
+PIPELINE_ENABLED = False
+
+PIPELINE_CSS = {
+    'base': {
+        'source_filenames': (
+          'css/core.css',
+          'css/colors/*.css',
+          'css/layers.css'
+        ),
+        'output_filename': 'css/colors.css',
+        'extra_context': {
+            'media': 'screen,projection',
+        },
+    },
+}
+
+PIPELINE_JS = {
+    'base': {
+        'source_filenames': (
+          'js/jquery.js',
+          'js/d3.js',
+          'js/collections/*.js',
+          'js/application.js',
+        ),
+        'output_filename': 'js/stats.js',
+    }
+}
+
+PIPELINE_COMPILERS = (
+    'pipeline.compilers.sass.SASSCompiler',
+)
