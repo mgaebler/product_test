583286de1c4d5ee971463f46eceadc90dae383be
Marian Gaebler <marian.gaebler@intosite.de>
2014-09-22 17:02:18 +0200

    faq admin and model

--- trendsetter/faq/admin.py
+++ trendsetter/faq/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import FaqGroup, FaqEntry
 
-# Register your models here.
+
+class FaqEntryRelation(admin.TabularInline):
+    model = FaqEntry
+    extra = 1
+
+
+class FaqGroupAdmin(admin.ModelAdmin):
+    inlines = (FaqEntryRelation,)
+
+
+admin.site.register(FaqGroup, FaqGroupAdmin)
+
+# admin.site.register(FaqEntry)
--- trendsetter/faq/models.py
+++ trendsetter/faq/models.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from markupfield.fields import MarkupField
 
 
 class FaqGroup(models.Model):
     name = models.CharField(max_length=255)
-
-    class Meta:
-        verbose_name = u'FAQ name'
-        ordering = ('-published',)
+    text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    last_modified_date = models.DateTimeField(auto_now=True, editable=False)
 
 
 class FaqEntry(models.Model):
     group = models.ForeignKey(FaqGroup)
-    question = models.TextField()
-    answer = models.TextField()
+    question = MarkupField()
+    #answer = MarkupField()
     creation_date = models.DateTimeField(auto_now_add=True, editable=False)
     last_modified_date = models.DateTimeField(auto_now=True, editable=False)
 
-    def __unicode__(self):
-        return ''
 
