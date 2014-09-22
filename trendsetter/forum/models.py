51f06bd12bedea18e3f3404c8dacc60133b9a255
Marian Gaebler <marian.gaebler@intosite.de>
2014-09-22 17:02:36 +0200

    forum models

--- trendsetter/forum/models.py
+++ trendsetter/forum/models.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from markupfield.fields import MarkupField
 
-# Create your models here.
+
+class ForumGroup(models.Model):
+    name = models.CharField(max_length=255)
+    text = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    last_modified_date = models.DateTimeField(auto_now=True, editable=False)
+
+
+class ForumEntry(models.Model):
+    group = models.ForeignKey(ForumGroup)
+    question = MarkupField()
+    #answer = MarkupField()
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    last_modified_date = models.DateTimeField(auto_now=True, editable=False)
+
+    def __unicode__(self):
+        return ''
+
