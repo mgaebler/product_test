94a6954ffe265ab100b46254029bbc1a420a447e
Marian Gaebler <marian.gaebler@intosite.de>
2014-09-22 11:20:06 +0200

    add static content

--- trendsetter/urls.py
+++ trendsetter/urls.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', TemplateView.as_view(template_name='index.html.jinja'), name='home'),
-    url(r'^foo/$', TemplateView.as_view(template_name='index.html'), name='foo'),
+    # static content
+    url(r'^$', TemplateView.as_view(template_name='index.jinja'), name='home'),
+    url(r'^ueber-uns/$', TemplateView.as_view(template_name='ueber_uns.jinja'), name='ueber_uns'),
+    url(r'^unsere-marken/$', TemplateView.as_view(template_name='unsere_marken.jinja'), name='unsere_marken'),
+    url(r'^wie-funktioniert-es/$', TemplateView.as_view(template_name='wie_funktioniert_es.jinja'), name='wie_funktioniert_es'),
+
     # url(r'^blog/', include('blog.urls')),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
