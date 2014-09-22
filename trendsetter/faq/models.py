b6a033ce77e64f3af16cc37ddead41f270400807
Marian Gaebler <marian.gaebler@intosite.de>
2014-09-22 14:30:22 +0200

    faq approach

--- trendsetter/faq/models.py
+++ trendsetter/faq/models.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class FaqGroup(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = u'FAQ name'
+        ordering = ('-published',)
+
+
+class FaqEntry(models.Model):
+    group = models.ForeignKey(FaqGroup)
+    question = models.TextField()
+    answer = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True, editable=False)
+    last_modified_date = models.DateTimeField(auto_now=True, editable=False)
+
+    def __unicode__(self):
+        return ''
+
