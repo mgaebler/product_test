594376868577faefe807261c6ff8950ebc457eb5
Marian Gaebler <marian.gaebler@intosite.de>
2014-09-23 16:14:25 +0200

    add product test model

--- trendsetter/product_test/models.py
+++ trendsetter/product_test/models.py
@@ -1,3 +1,41 @@
+# coding: utf8
 from django.db import models
 
-# Create your models here.
+
+class Brands(models.Model):
+    pass
+
+
+class Participations(models.Model):
+    pass
+
+
+class Invites(models.Model):
+    pass
+
+
+class ProductTest(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
+    brand = models.ForeignKey(Brands)
+    # Images
+    hero_image = models.FileField()
+    hero_image_url = models.URLField()
+    list_image = models.FileField()
+    list_image_url = models.URLField()
+    logo = models.FileField()
+    logo_url = models.URLField()
+    # Customization
+    custom_html = models.TextField()
+    custom_css = models.TextField()
+    # Dates
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField()
+    ends_at = models.DateTimeField()
+    activated_at = models.DateTimeField()
+
+    state = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.title
