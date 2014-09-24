12d4be552f3c7fd08ce313dfed7fc677d37e1c6b
Marian Gaebler <marian.gaebler@intosite.de>
2014-09-24 11:33:04 +0200

    login

--- trendsetter/profiles/views.py
+++ trendsetter/profiles/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 
+def login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            # Redirect to a success page.
+        else:
+            # Return a 'disabled account' error message
+            pass
+    else:
+        # Return an 'invalid login' error message.
+        pass
 
+
+def logout_view(request):
+    logout(request)
--- trendsetter/urls.py
+++ trendsetter/urls.py
@@ -26,7 +26,8 @@ urlpatterns = patterns('',
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += patterns('',
+    urlpatterns += patterns(
+        '',
         url(r'^__debug__/', include(debug_toolbar.urls)),
     )
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
