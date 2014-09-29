8e10d5ef804fdf63cffc0c60bb1abe8c5e5e0cc3
Marian Gaebler <marian.gaebler@intosite.de>
2014-09-29 12:20:52 +0200

    many template changes

--- trendsetter/profiles/urls.py
+++ trendsetter/profiles/urls.py
@@ -3,11 +3,11 @@ from django.views.generic import TemplateView
 from views import login_view, logout_view, password_change
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='profiles/product_tests.jinja'), name='profile_index'),
+    url(r'^$', TemplateView.as_view(template_name='profiles/my_site/product_tests.jinja'), name='profile_index'),
     url(r'^profil$', TemplateView.as_view(template_name='profiles/settings.jinja'), name='profile_settings'),
-    url(r'^tests$', TemplateView.as_view(template_name='profiles/product_tests.jinja'), name='profile_tests'),
-    url(r'^trendpoints$', TemplateView.as_view(template_name='profiles/trendpoints.jinja'), name='profile_trendpoints'),
-    url(r'^freunde-einladen$', TemplateView.as_view(template_name='profiles/invite_friends.jinja'), name='profile_invite_friends'),
+    url(r'^tests$', TemplateView.as_view(template_name='profiles/my_site/product_tests.jinja'), name='profile_tests'),
+    url(r'^trendpoints$', TemplateView.as_view(template_name='profiles/my_site/trendpoints.jinja'), name='profile_trendpoints'),
+    url(r'^freunde-einladen$', TemplateView.as_view(template_name='profiles/my_site/invite_friends.jinja'), name='profile_invite_friends'),
 
     url(r'^register/$', login_view, name='user_register'),
     url(r'^preregister/$', login_view, name='user_preregister'),
