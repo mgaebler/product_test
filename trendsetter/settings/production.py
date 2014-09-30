c5504069584f61915805d9f39d52ffe1b83ca957
Marian Gaebler <marian.gaebler@intosite.de>
2014-09-30 21:34:26 +0200

    update production settings

--- trendsetter/settings/production.py
+++ trendsetter/settings/production.py
@@ -4,6 +4,10 @@ from .common import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+THUMBNAIL_DEBUG = False
 # Template Settings
 TEMPLATE_DEBUG = False
+DEBUG_TOOLBAR = False
+
+# https://django-pipeline.readthedocs.org/en/latest/storages.html
+STATICFILES_STORAGE = 'pipeline.storage.NonPackagingPipelineCachedStorage'
