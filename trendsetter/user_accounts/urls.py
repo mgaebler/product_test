60ec6ca28a8c1fc9c85cb0bb35c6493fb4ef23ad
Marian Gaebler <marian.gaebler@intosite.de>
2014-09-30 18:23:27 +0200

    make use of namespaces

--- trendsetter/user_accounts/urls.py
+++ trendsetter/user_accounts/urls.py
@@ -4,14 +4,14 @@ from views import login_view, logout_view, password_change, RegistrationFormView
 
 urlpatterns = patterns(
     '',
-    url(r'^$', TemplateView.as_view(template_name='profiles/my_site/product_tests.jinja'), name='profile_index'),
-    url(r'^profil$', UserProfileFormView.as_view(template_name='profiles/settings/profile.jinja'), name='profile_settings'),
-    url(r'^tests$', TemplateView.as_view(template_name='profiles/my_site/product_tests.jinja'), name='profile_tests'),
-    url(r'^trendpoints$', TemplateView.as_view(template_name='profiles/my_site/trendpoints.jinja'), name='profile_trendpoints'),
-    url(r'^freunde-einladen$', TemplateView.as_view(template_name='profiles/my_site/invite_friends.jinja'), name='profile_invite_friends'),
+    url(r'^$', TemplateView.as_view(template_name='profiles/my_site/product_tests.jinja'), name='index'),
+    url(r'^profil$', UserProfileFormView.as_view(template_name='profiles/settings/profile.jinja'), name='settings'),
+    url(r'^tests$', TemplateView.as_view(template_name='profiles/my_site/product_tests.jinja'), name='tests'),
+    url(r'^trendpoints$', TemplateView.as_view(template_name='profiles/my_site/trendpoints.jinja'), name='trendpoints'),
+    url(r'^freunde-einladen$', TemplateView.as_view(template_name='profiles/my_site/invite_friends.jinja'), name='invite_friends'),
 
-    url(r'^register/$', RegistrationFormView.as_view(template_name='profiles/register_form.jinja'), name='user_register'),
-    url(r'^preregister/$', login_view, name='user_preregister'),
+    url(r'^register/$', RegistrationFormView.as_view(template_name='profiles/register_form.jinja'), name='register'),
+    url(r'^preregister/$', login_view, name='preregister'),
     url(r'^login-form/$', TemplateView.as_view(template_name='profiles/login_form.jinja'), name='login_form'),
     url(r'^login/$', login_view, name='login'),
     url(r'^logout/$', logout_view, name='logout'),
