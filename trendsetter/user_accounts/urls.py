6b27794c4fea0ac4178811554e1034988b2fd144
Marian Gaebler <marian.gaebler@intosite.de>
2014-10-01 20:05:40 +0200

    namespace fixes

--- trendsetter/user_accounts/urls.py
+++ trendsetter/user_accounts/urls.py
@@ -10,11 +10,11 @@ urlpatterns = patterns(
     url(r'^trendpoints$', TemplateView.as_view(template_name='profiles/my_site/trendpoints.jinja'), name='trendpoints'),
     url(r'^freunde-einladen$', TemplateView.as_view(template_name='profiles/my_site/invite_friends.jinja'), name='invite_friends'),
 
-    url(r'^register/$', views.RegistrationFormView.as_view(template_name='profiles/register_form.jinja'), name='register'),
-    url(r'^preregister/$', views.login_view, name='preregister'),
-    url(r'^login-form/$', TemplateView.as_view(template_name='profiles/login_form.jinja'), name='login_form'),
-    url(r'^login/$', views.login_view, name='login'),
-    url(r'^logout/$', views.logout_view, name='logout'),
-    url(r'^change-password/$', TemplateView.as_view(template_name='index.jinja'), name='change_password'),
-    url(r'^forgot-password/$', TemplateView.as_view(template_name='index.jinja'), name='forgot_password'),
+    url(r'^register$', views.RegistrationFormView.as_view(template_name='profiles/register_form.jinja'), name='register'),
+    url(r'^preregister$', views.login_view, name='preregister'),
+    url(r'^login-form$', TemplateView.as_view(template_name='profiles/login_form.jinja'), name='login_form'),
+    url(r'^login$', views.login_view, name='login'),
+    url(r'^logout$', views.logout_view, name='logout'),
+    url(r'^change-password$', TemplateView.as_view(template_name='index.jinja'), name='change_password'),
+    url(r'^forgot-password$', TemplateView.as_view(template_name='index.jinja'), name='forgot_password'),
 )
