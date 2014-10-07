c2642f67045410bee85c1781de9f710a4f7e1d62
Marian Gaebler <marian.gaebler@intosite.de>
2014-10-07 15:50:45 +0200

    migrations

--- trendsetter/user_accounts/migrations/0001_initial.py
+++ trendsetter/user_accounts/migrations/0001_initial.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 import django.utils.timezone
 from django.conf import settings
-import django.core.validators
 import easy_thumbnails.fields
 
 
@@ -22,13 +21,12 @@ class Migration(migrations.Migration):
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(default=django.utils.timezone.now, verbose_name='last login')),
                 ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
-                ('username', models.CharField(help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', unique=True, max_length=30, verbose_name='username', validators=[django.core.validators.RegexValidator('^[\\w.@+-]+$', 'Enter a valid username.', 'invalid')])),
-                ('first_name', models.CharField(max_length=30, verbose_name='first name', blank=True)),
-                ('last_name', models.CharField(max_length=30, verbose_name='last name', blank=True)),
-                ('email', models.EmailField(max_length=75, verbose_name='email address', blank=True)),
+                ('email', models.EmailField(unique=True, max_length=255, verbose_name='email address', db_index=True)),
                 ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
-                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active.  Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('full_name', models.CharField(max_length=255, verbose_name=b'full name', blank=True)),
+                ('preferred_name', models.CharField(max_length=255, verbose_name=b'preferred name', blank=True)),
                 ('gender', models.CharField(max_length=1, choices=[(b'm', 'male'), (b'f', 'female'), (b'-', 'something else')])),
                 ('avatar', easy_thumbnails.fields.ThumbnailerImageField(null=True, upload_to=b'', blank=True)),
                 ('avatar_url', models.URLField(max_length=254, null=True, blank=True)),
@@ -50,9 +48,8 @@ class Migration(migrations.Migration):
                 ('user_permissions', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')),
             ],
             options={
+                'ordering': ['email'],
                 'abstract': False,
-                'verbose_name': 'user',
-                'verbose_name_plural': 'users',
             },
             bases=(models.Model,),
         ),
