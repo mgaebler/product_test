0181fc1c8f7e44da564f39cf81275cd4bf1ed62a
Marian Gaebler <marian.gaebler@intosite.de>
2014-10-07 15:49:27 +0200

    remove user profile form

--- trendsetter/user_accounts/forms.py
+++ trendsetter/user_accounts/forms.py
@@ -10,34 +10,3 @@ class LoginForm(forms.Form):
         widget=forms.PasswordInput(render_value=False),
         max_length=100
     )
-
-
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserAccount
-        fields = (
-            'username',
-            'avatar',
-            'email',
-            # 'password',
-            'first_name',
-            'last_name',
-            'address1',
-            'address2',
-            'address3',
-            'postcode',
-            'city',
-            'country',
-            # personal data
-            'birth_date',
-            'gender',
-            'family_status',
-        )
-        labels = {
-            'address2': '',
-            'address3': '',
-        },
-        help_texts = {
-            'avatar': _(u'Max. 10MB. Wird öffentlich angezeigt.'),
-            'password': _(u'Nur, wenn Du Dein Passwort ändern willst.'),
-        }
