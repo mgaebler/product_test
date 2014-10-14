ecc81af0282b6333bfa4eb3860fd1a02c13bfd33
Marian Gaebler <marian.gaebler@intosite.de>
2014-10-14 10:28:17 +0200

    add shop backend

--- trendsetter/simple_shop/admin.py
+++ trendsetter/simple_shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(models.Product, ProductAdmin)
