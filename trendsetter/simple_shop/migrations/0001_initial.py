e2fe87996a6897884c7918ae639119803e18704d
Marian Gaebler <marian.gaebler@intosite.de>
2014-10-14 10:33:08 +0200

    migrations

--- trendsetter/simple_shop/migrations/0001_initial.py
+++ trendsetter/simple_shop/migrations/0001_initial.py
@@ -12,13 +12,15 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='Products',
+            name='Product',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=254)),
+                ('slug', models.SlugField()),
                 ('description', models.TextField()),
-                ('photo', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'')),
+                ('image', easy_thumbnails.fields.ThumbnailerImageField(upload_to=b'shop')),
                 ('value', models.IntegerField()),
+                ('active', models.BooleanField(default=True)),
                 ('updated', models.DateTimeField(auto_now=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
             ],
