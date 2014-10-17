beaf096d76df2bd3afb3a97b09c628ebf80bfefc
Marian Gaebler <marian.gaebler@intosite.de>
2014-10-17 08:39:53 +0200

    add staging config

--- django_app/settings/staging.py
+++ django_app/settings/staging.py
@@ -1,3 +1,23 @@
 from .common import *
+# development stuff here
 
-# staging settings here
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+THUMBNAIL_DEBUG = True
+# Template Settings
+TEMPLATE_DEBUG = False
+
+
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
+
+INSTALLED_APPS += (
+    'debug_toolbar.apps.DebugToolbarConfig',
+)
+
+# MIDDLEWARE_CLASSES += (
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# )
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
