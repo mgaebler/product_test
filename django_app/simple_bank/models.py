60804d287292a2b5fb6446d9cd067c0db0348046
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-03 11:50:09 +0100

    add overdraft logic

--- django_app/simple_bank/models.py
+++ django_app/simple_bank/models.py
@@ -1,9 +1,14 @@
 # coding: utf8
+import logging
 
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.conf import settings
 from django.utils.translation import ugettext as _
 from django.db.models.signals import post_save
+
+
+log = logging.getLogger(__name__)
 
 
 class Account(models.Model):
@@ -17,19 +22,27 @@ class Account(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
     type = models.CharField(max_length=2, choices=TYPE_CHOICES, default='ca')
     description = models.TextField(blank=True, null=True)
-    customer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='bank_account')
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='bank_account', null=True, blank=True)
     balance = models.IntegerField(default=0)
+    overdraft = models.IntegerField(default=0, help_text=_(u'Limit of overdraft the current account.'))
     currency = models.CharField(max_length=6, choices=CURRENCY_CHOICES, default='tp')
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    def debit(self, account):
-        pass
+    def get_possible_amount(self):
+        #todo: Of cause this is a bad name for this function. Please think over and refactor.
+        return self.balance + self.overdraft
 
     def credit(self, account):
+        # give credit
+        pass
+
+    def debit(self, account):
+        # make debit
         pass
 
     def validate_balance(self):
+        # check balance based on the transfers
         pass
 
     def __unicode__(self):
@@ -45,12 +58,35 @@ class Transfer(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        # Don't allow transfers below the credit limit.
+
+        if (self.sender.get_possible_amount() - self.amount) < 0 and not self.sender.type == 'ha':
+            log.warning(_(u'Balance limit of {} is reached. Limit is:'.format(self.sender, self.sender.get_possible_amount())))
+            return False
+        elif self.sender.type == 'ha':
+            print '--- house account ---'
+            log.info(_('Debit by house account'))
+            super(Transfer, self).save(force_insert, force_update, using, update_fields)
+        else:
+            super(Transfer, self).save(force_insert, force_update, using, update_fields)
+
+    # def clean(self):
+    #     # Don't allow transfers below the credit limit.
+    #     if (self.sender.get_possible_amount() - self.amount) < 0:
+    #         log.warning(_(u'Balance limit of {} is reached. Limit is:'.format(self.sender, self.sender.get_possible_amount())))
+    #         raise ValidationError('Balance limit is reached.')
+
+
+    def __unicode__(self):
+        return u"{} -> {}".format(self.sender, self.receiver)
 
 def create_customer_account(sender, **kwargs):
     # use the user instance to create a new account if it not already exists
     user_account = kwargs.get('instance', None)
     if not user_account.bank_account.all():
         Account.objects.create(customer=user_account)
+
 
 post_save.connect(create_customer_account, sender=settings.AUTH_USER_MODEL, dispatch_uid="create_user_profile")
 
@@ -63,6 +99,8 @@ def create_transfer(sender_account, receiver_account, amount, message):
     transfer.receiver = receiver_account
     transfer.amount = amount
     transfer.save()
+
+    return transfer
 
 
 def execute_transfer(sender, **kwargs):
