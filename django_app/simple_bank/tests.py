b2822814714bf39b863e6e2fd09e79533dd84ae0
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-03 11:52:08 +0100

    add bank tests

--- django_app/simple_bank/tests.py
+++ django_app/simple_bank/tests.py
@@ -1,21 +1,27 @@
 import factory
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from simple_bank.models import create_transfer
+from simple_bank.models import create_transfer, Account
 
 
-class UserFactory(factory.django.DjangoModelFactory):
+class CustomerFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = get_user_model()
     preferred_name = factory.Sequence(lambda n: 'user%d' % n)
     email = factory.LazyAttribute(lambda obj: '%s@example.com' % obj.preferred_name)
 
 
-class BankAccountTestCase(TestCase):
+class CustomerAccountTestCase(TestCase):
     def setUp(self):
         # todo: Create user and bank accounts
-        self.user1 = UserFactory.create()
-        self.user2 = UserFactory.create()
+        self.user1 = CustomerFactory.create()
+        self.user2 = CustomerFactory.create()
+
+        self.house_account = Account.objects.create(
+            name='HouseAccount',
+            type='ha',
+            description='A house account for the app.'
+        )
 
     def test_if_bank_account_exists(self):
         # todo: After a user account is created the user should also have equivalent a bank account.
@@ -24,12 +30,15 @@ class BankAccountTestCase(TestCase):
     def test_if_amount_transfer_is_valid(self):
         # Do a transaction
         # todo: make sure that the senders debit is the receivers credit.
+        transfer_amount = 10
         account1 = self.user1.bank_account.all().first()
+        account1.balance = transfer_amount
+        account1.save()
         account2 = self.user2.bank_account.all().first()
 
-        transfer_amount = 10
         # user1 sends user2 ten bugs
-        create_transfer(
+        # the transfer should fail
+        transfer = create_transfer(
             sender_account=account1,
             receiver_account=account2,
             amount=transfer_amount,
@@ -38,7 +47,7 @@ class BankAccountTestCase(TestCase):
 
         self.assertEqual(account2.balance, transfer_amount)
 
-    def test_if_user_accounts_can_have_negative_balance(self):
+    def test_user_accounts_should_not_overdraft(self):
         # User accounts should not have negative balance.
         # todo: Try to spend more than your amount.
         account1 = self.user1.bank_account.all().first()
@@ -46,6 +55,7 @@ class BankAccountTestCase(TestCase):
 
         transfer_amount = 10
         # user1 sends user2 ten bugs
+        # this is ok but should fail
         create_transfer(
             sender_account=account1,
             receiver_account=account2,
@@ -53,9 +63,24 @@ class BankAccountTestCase(TestCase):
             message='test message'
         )
 
-        self.assertGreaterEqual(account1.balance, 0)
+        # The transfer should fail and booth account amounts should be 0
+        self.assertEqual(account1.balance, 0)
+        self.assertEqual(account2.balance, 0)
 
-    def test_if_house_accounts_can_have_negative_balance(self):
+    def test_if_house_accounts_can_make_negative_balance(self):
         # House accounts should have negative balance.
         # todo: Try to spend more than your amount.
-        pass
+        transfer_amount = 10
+        account1 = self.house_account
+        account2 = self.user2.bank_account.all().first()
+
+        # user1 sends user2 ten bugs
+        # the transfer should fail
+        transfer = create_transfer(
+            sender_account=account1,
+            receiver_account=account2,
+            amount=transfer_amount,
+            message='test message'
+        )
+
+        self.assertEqual(account2.balance, transfer_amount)
