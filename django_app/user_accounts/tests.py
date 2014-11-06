80d5fb63447cd99894563a383d5843ce2dadbcf7
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-06 09:42:34 +0100

    test notes

--- django_app/user_accounts/tests.py
+++ django_app/user_accounts/tests.py
@@ -1,1 +1,20 @@
 from django.test import TestCase
+
+
+class UserRegistrationTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_if_registration_link_is_only_7_days_valid(self):
+        pass
+
+
+class UserInviteTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_if_invite_link_is_valid(self):
+        pass
+
+    def test_if_invite_user_is_set(self):
+        pass
