2c956af79f52ac90029a3b1777f96e69af85a56b
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-06 18:25:39 +0100

    configure db for staging

--- django_app/settings/staging.py
+++ django_app/settings/staging.py
@@ -1,6 +1,10 @@
 from .common import *
 # development stuff here
 
+ansible_vars = {}
+with file(os.path.join(BASE_DIR, 'ansible', 'group_vars', 'staging.yml')) as f:
+    ansible_vars = yaml.load(f.read())
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 THUMBNAIL_DEBUG = True
@@ -24,3 +28,14 @@ INSTALLED_APPS += (
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail00.exor'
 DEFAULT_FROM_EMAIL = 'staging@trendsetter.eu'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': ansible_vars['app']['id'],
+        'USER': ansible_vars['app']['id'],
+        'PASSWORD': ansible_vars['app']['id'],
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
