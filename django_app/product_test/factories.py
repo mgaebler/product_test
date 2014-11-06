567ea289f109a58cac49b465b667f3f24b189c66
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-06 14:17:51 +0100

    product test factory can produce it's own images

--- django_app/product_test/factories.py
+++ django_app/product_test/factories.py
@@ -1,7 +1,18 @@
 # coding: utf8
 import factory
+from PIL import Image
+from StringIO import StringIO
+from django.core.files.base import ContentFile
+from product_test import models
 
-from . import models
+
+def create_image():
+    image_file = StringIO()
+    image = Image.new('RGBA', size=(50,50), color=(256,0,0))
+    image.save(image_file, 'png')
+    image_file.seek(0)
+
+    return ContentFile(image_file.read(), 'test.png')
 
 
 class ProductTestFactory(factory.django.DjangoModelFactory):
@@ -9,4 +20,7 @@ class ProductTestFactory(factory.django.DjangoModelFactory):
         model = models.ProductTest
 
     title = factory.Sequence(lambda n: "TestTitle #%s" % n)
-    slug = factory.Sequence(lambda n: "test_title_%s" % n)
+    slug = factory.Sequence(lambda n: "test-title-%s" % n)
+    hero_image = create_image()
+    list_image = create_image()
+    logo = create_image()
