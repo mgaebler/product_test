6cd3fbb0a054eb06c043491751da8e98a3933c4b
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-07 20:41:20 +0100

    disabled assets pipeline in staging mode

--- django_app/settings/staging.py
+++ django_app/settings/staging.py
@@ -11,6 +11,9 @@ THUMBNAIL_DEBUG = True
 # Template Settings
 TEMPLATE_DEBUG = False
 
+# ok, this is a bit weird. PIPELINE_ENABLED = True means, if debug is true pipeline is off.
+# So to re-enable it just comment the next line.
+PIPELINE_ENABLED = True
 
 INTERNAL_IPS = (
     '127.0.0.1',
