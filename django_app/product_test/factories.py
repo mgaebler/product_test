2af8d1f9f2dec8601bdf1b31008969b927738c64
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-07 09:46:36 +0100

    use the factory approach to create images

--- django_app/product_test/factories.py
+++ django_app/product_test/factories.py
@@ -1,18 +1,6 @@
 # coding: utf8
 import factory
-from PIL import Image
-from StringIO import StringIO
-from django.core.files.base import ContentFile
 from product_test import models
-
-
-def create_image():
-    image_file = StringIO()
-    image = Image.new('RGBA', size=(50,50), color=(256,0,0))
-    image.save(image_file, 'png')
-    image_file.seek(0)
-
-    return ContentFile(image_file.read(), 'test.png')
 
 
 class ProductTestFactory(factory.django.DjangoModelFactory):
@@ -21,6 +9,8 @@ class ProductTestFactory(factory.django.DjangoModelFactory):
 
     title = factory.Sequence(lambda n: "TestTitle #%s" % n)
     slug = factory.Sequence(lambda n: "test-title-%s" % n)
-    hero_image = create_image()
-    list_image = create_image()
-    logo = create_image()
+    hero_image = factory.django.ImageField(color='green')
+    list_image = factory.django.ImageField(color='blue')
+    logo = factory.django.ImageField(color='red')
+
+
