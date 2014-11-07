f34e14af6b81177dee97ff7bec2cb9f1bd92b913
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-07 12:03:54 +0100

    removed super useless markup field

--- django_app/faq/models.py
+++ django_app/faq/models.py
@@ -2,8 +2,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from markupfield.fields import MarkupField
-
 
 class FaqGroup(models.Model):
     name = models.CharField(max_length=255)
@@ -11,12 +9,19 @@ class FaqGroup(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True, editable=False)
     last_modified_date = models.DateTimeField(auto_now=True, editable=False)
 
+    def __unicode__(self):
+        return self.name
+
 
 class FaqEntry(models.Model):
     group = models.ForeignKey(FaqGroup)
-    question = MarkupField()
-    #answer = MarkupField()
+    question = models.TextField()
+    answer = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True, editable=False)
     last_modified_date = models.DateTimeField(auto_now=True, editable=False)
+    is_active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.question
 
 
