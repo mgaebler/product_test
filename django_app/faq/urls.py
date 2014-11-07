43b42a74da41e5774198c5f06165ef8f3dcd0893
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-07 12:05:47 +0100

    faq urls

---
+++ django_app/faq/urls.py
@@ -0,0 +1,10 @@
+# coding: utf8
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+
+from . import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', TemplateView.as_view(template_name='product_test/faq.jinja'), name='faq'),
+)
