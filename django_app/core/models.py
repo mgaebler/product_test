7b9fb5244fcc81cc41006c8ab19b043695d93f97
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-17 12:17:02 +0100

    add static pages models

--- django_app/core/models.py
+++ django_app/core/models.py
@@ -1,1 +1,12 @@
-__author__ = 'marian.gaebler'
+# coding: utf-8
+from django.db import models
+
+
+class StaticPage(models.Model):
+    name = models.CharField(max_length=254)
+    slug = models.SlugField()
+    description = models.TextField()
+    content = models.TextField()
+
+    def __unicode__(self):
+        return self.name
