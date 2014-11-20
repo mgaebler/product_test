805e4ff5d917abdb513ebec670b2739c59185593
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-20 17:53:02 +0100

    expanded user list in admin area it's also possible to search users

--- django_app/user_accounts/admin.py
+++ django_app/user_accounts/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import UserAccount
 
 
-admin.site.register(UserAccount)
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name', 'preferred_name', 'profile_complete',)
+    search_fields = ('email', 'full_name', 'preferred_name', 'profile_complete',)
+
+admin.site.register(UserAccount, UserAccountAdmin)
 
