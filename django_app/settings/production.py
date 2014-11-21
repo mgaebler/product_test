8ce1015cfefeda6de33cd75f97f40ee47fc34c92
Marian Gaebler <marian.gaebler@gmail.com>
2014-11-21 19:15:39 +0100

    update production settings

--- django_app/settings/production.py
+++ django_app/settings/production.py
@@ -1,9 +1,6 @@
 import yaml
 from .common import *
 
-ansible_vars = {}
-with file(os.path.join(BASE_DIR, 'ansible', 'group_vars', 'production.yml')) as f:
-    ansible_vars = yaml.load(f.read())
 
 # production settings here
 WSGI_APPLICATION = 'django_app.wsgi.production.application'
@@ -17,11 +14,11 @@ TEMPLATE_DEBUG = False
 DEBUG_TOOLBAR = False
 
 # https://django-pipeline.readthedocs.org/en/latest/storages.html
-STATICFILES_STORAGE = 'pipeline.storage.NonPackagingPipelineCachedStorage'
+# STATICFILES_STORAGE = 'pipeline.storage.NonPackagingPipelineCachedStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail00.exor'
-DEFAULT_FROM_EMAIL = 'staging@trendsetter.eu'
+DEFAULT_FROM_EMAIL = 'info@trendsetter.eu'
 
 DATABASES = {
     'default': {
