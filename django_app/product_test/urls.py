c6c3e419b5d067e87ab2625417c579de26b18b54
Marian Gaebler <marian.gaebler@gmail.com>
2014-12-04 22:25:55 +0100

    make galleries work

--- django_app/product_test/urls.py
+++ django_app/product_test/urls.py
@@ -8,10 +8,7 @@ urlpatterns = patterns(
     url(r'^(?P<slug>[\w-]+)/info$', views.ProductTestDetail.as_view(template_name='product_test/info.jinja'), name='info'),
 
     url(r'^(?P<slug>[\w-]+)/forum', include('product_test.urls_forum', namespace='forum')),
-
-    url(r'^(?P<slug>[\w-]+)/galerie$', views.GalleryView.as_view(), name='gallery'),
-    url(r'^(?P<slug>[\w-]+)/galerie/image/upload$', views.GalleryView.as_view(), name='gallery_image_upload'),
-    url(r'^(?P<slug>[\w-]+)/galerie/video/upload$', views.GalleryView.as_view(), name='gallery_video_upload'),
+    url(r'^(?P<slug>[\w-]+)/galerie', include('product_test.urls_gallery', namespace='gallery')),
 
     url(r'^(?P<slug>[\w-]+)/faq$', views.ProductTestDetail.as_view(template_name='product_test/faq.jinja'), name='faq'),
     url(r'^brands$', views.ProductBrandListView.as_view(), name='brand_list'),
