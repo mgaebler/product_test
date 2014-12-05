18c94200f3dee337e7c327e29b53f951722cdd6a
Marian Gaebler <marian.gaebler@gmail.com>
2014-12-05 09:10:00 +0100

    Add Gallery admin

--- django_app/gallery/admin.py
+++ django_app/gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from gallery.models import Gallery, GalleryImage
 
-# Register your models here.
+
+admin.site.register(Gallery)
+admin.site.register(GalleryImage)
+
+
