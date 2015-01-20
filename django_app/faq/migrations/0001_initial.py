e3bf242ccdb9cfd11e68135a430e373a8672c200
Marian Gaebler <marian.gaebler@gmail.com>
2015-01-20 17:12:51 +0100

    fixed migrations

--- django_app/faq/migrations/0001_initial.py
+++ django_app/faq/migrations/0001_initial.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import markupfield.fields
 
 
 class Migration(migrations.Migration):
@@ -15,9 +14,9 @@ class Migration(migrations.Migration):
             name='FaqEntry',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('question', markupfield.fields.MarkupField()),
+                ('question', models.CharField(default=None, max_length=255)),
                 ('question_markup_type', models.CharField(default=None, max_length=30, choices=[(b'', b'--'), (b'markdown', b'markdown')])),
-                ('answer', markupfield.fields.MarkupField()),
+                ('answer', models.CharField(default=None, max_length=255)),
                 ('_question_rendered', models.TextField(editable=False)),
                 ('creation_date', models.DateTimeField(auto_now_add=True)),
                 ('answer_markup_type', models.CharField(default=None, max_length=30, choices=[(b'', b'--'), (b'markdown', b'markdown')])),
