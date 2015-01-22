97df2df63c4d591de0e67ebe530ba6c09fdcbb95
Marian Gaebler <marian.gaebler@gmail.com>
2015-01-22 16:05:47 +0100

    typo

--- django_app/static_pages/models.py
+++ django_app/static_pages/models.py
@@ -16,7 +16,7 @@ class FlatPage(models.Model):
     content = models.TextField(_('content'), blank=True)
     enable_comments = models.BooleanField(_('enable comments'), default=False)
     template_name = models.CharField(_('template name'), max_length=70, blank=True,
-        help_text=_("Example: 'flatpages/contact_page.html'. If this isn't provided, the system will use 'flatpages/default.html'."))
+        help_text=_("Example: 'static_pages/contact_page.html'. If this isn't provided, the system will use 'static_pages/default.html'."))
     registration_required = models.BooleanField(_('registration required'),
         help_text=_("If this is checked, only logged-in users will be able to view the page."),
         default=False)
