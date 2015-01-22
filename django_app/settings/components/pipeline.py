2696a2454fc573100b0a9431abf36e2e7daccbda
Marian Gaebler <marian.gaebler@gmail.com>
2015-01-22 14:55:26 +0100

    removed unused configs

--- django_app/settings/components/pipeline.py
+++ django_app/settings/components/pipeline.py
@@ -39,11 +39,11 @@ PIPELINE_COMPILERS = (
 )
 
 # PIPELINE_CSS_COMPRESSOR = 'pipeline.compressors.NoopCompressor'
-PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.NoopCompressor'
+# PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.NoopCompressor'
 PIPELINE_CSS_COMPRESSOR = 'pipeline.compressors.yuglify.YuglifyCompressor'
 # PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.yuglify.YuglifyCompressor'
 PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.jsmin.JSMinCompressor'
 
-MANIFESTO_EXCLUDED_MANIFESTS = (
+# MANIFESTO_EXCLUDED_MANIFESTS = (
         # 'randomapp.manifest.WrongManifest',
-)
+# )
