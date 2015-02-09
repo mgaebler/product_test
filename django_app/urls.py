3b84664ea5ed54b0f364f314c70f61615679cb63
Marian Gaebler <marian.gaebler@gmail.com>
2015-02-09 16:40:39 +0100

    Updated static page urls and corresponding footer links. Those links are not dynamic so you have to make sure they match correctly.

--- django_app/urls.py
+++ django_app/urls.py
@@ -15,12 +15,6 @@ urlpatterns = patterns('',
     url(r'^ueber-uns/$', TemplateView.as_view(template_name='static/ueber_uns.jinja'), name='about_us'),
     url(r'^unsere-marken/$', TemplateView.as_view(template_name='static/unsere_marken.jinja'), name='our_brands'),
     url(r'^wie-funktioniert-es/$', TemplateView.as_view(template_name='static/wie_funktioniert_es.jinja'), name='how_it_works'),
-    # url(r'^agb/$', TemplateView.as_view(template_name='static/agb.jinja'), name='agb'),
-    url(r'^teilnahmebedingungen/$', TemplateView.as_view(template_name='static/teilnahmebedingungen.jinja'), name='eligibility_requirements'),
-    # url(r'^datenschutz/$', TemplateView.as_view(template_name='static/datenschutz.jinja'), name='datenschutz'),
-    # url(r'^faq/$', TemplateView.as_view(template_name='static/faq.jinja'), name='faq'),
-    # url(r'^impressum/$', TemplateView.as_view(template_name='static/impressum.jinja'), name='impressum'),
-    # url(r'^partner-info/$', TemplateView.as_view(template_name='static/partner_info.jinja'), name='partner_info'),
 
     url(r'my/', include('user_accounts.urls', namespace='user')),
     url(r'produkttests/', include('product_test.urls', namespace='product_test')),
@@ -30,11 +24,11 @@ urlpatterns = patterns('',
     url(r'^export/csv/confirmed-users', streaming_csv_view),
     url(r'^export/csv/get_gender_birth_date', get_gender_birth_date_csv),
 
-    url(r'', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/filebrowser/', include(site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
+    url(r'', include('social.apps.django_app.urls', namespace='social')),
     url(r'', include('static_pages.urls')),
 )
 
