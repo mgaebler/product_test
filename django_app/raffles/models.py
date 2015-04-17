289dc32d94b7c16ced2ff87806c05669fb0f5a49
Kai Diefenbach <kai.diefenbach@iqpp.de>
2015-04-17 11:39:13 +0200

    Checks whether there is an end date.

--- django_app/raffles/models.py
+++ django_app/raffles/models.py
@@ -9,13 +9,15 @@ from simple_comments.models import Comment
 
 class StartedRaffleManager(models.Manager):
     def started(self):
+        """
+        Returns only raffles which have a start date and the start date is in
+        past.
+        """
         qs = super(StartedRaffleManager, self).get_queryset()
         return qs.filter(starts_at__lte=timezone.now())
 
 
 class Raffle(models.Model):
-    """
-    """
     url = models.CharField(_('URL'), max_length=100, db_index=True)
     title = models.CharField(_('title'), max_length=200)
     content = models.TextField(_('content'), blank=True)
@@ -32,13 +34,16 @@ class Raffle(models.Model):
     class Meta:
         verbose_name = _('raffle')
         verbose_name_plural = _('raffles')
-        ordering = ('starts_at',)
+        ordering = ('starts_at', 'ends_at')
 
     def __unicode__(self):
         return "%s -- %s" % (self.url, self.title)
 
     def is_expired(self):
-        return self.ends_at < timezone.now()
+        """
+        Raffle is expired if there is an end date and the end date is in past.
+        """
+        return self.ends_at and (self.ends_at < timezone.now())
 
     def get_absolute_url(self):
         return reverse("raffles:detail", kwargs={"url": self.url})
