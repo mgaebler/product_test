7c12dda69a77f57535bd5c46e37f24cc1a36267a
Kai Diefenbach <kai.diefenbach@iqpp.de>
2015-04-23 15:01:17 +0200

    Removes WYSIWYG editor from comments.

--- django_app/raffles/admin.py
+++ django_app/raffles/admin.py
@@ -33,5 +33,5 @@ class RaffleAdmin(admin.ModelAdmin):
     class Media:
         js = [
             static('grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js'),
-            static('grappelli/tinymce_setup/tinymce_setup.js'),
+            static('raffles/tinymce_setup/tinymce_setup.js'),
         ]
