ea1c916c4470b83a75396af423a8268a6852b765
Kai Diefenbach <kai.diefenbach@iqpp.de>
2015-05-28 16:46:05 +0200

    Adds dynamic page ranges for forum posts.

--- django_app/product_test/views_forum.py
+++ django_app/product_test/views_forum.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+from collections import deque
+from itertools import count
 from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
@@ -40,6 +42,42 @@ class ForumDetailView(ForumBaseView):
 class TopicView(ForumBaseView):
     template_name = 'product_test/forum/topic.jinja'
 
+    def get_page_range(self, current_page, total_pages, window=4):
+        """
+        Returns range of pages that contains current page and few pages before and after it.
+        """
+        # maximum length of page range is window + 1
+        maxlen = window + 1
+        page_range = deque(maxlen=maxlen)
+
+        # minimum possible index is either: (current_page - window) or 1
+        window_start = (current_page - window) if (current_page - window) > 0 else 1
+
+        # maximum possible index is current_page + window or total_pages
+        window_end = total_pages if (current_page + window) > total_pages else (current_page + window)
+
+        # if we have enough pages then we should end at preferred end
+        preferred_end = current_page + int(window / 2.0)
+
+        for i in count(window_start):
+            if i > window_end:
+                # if we're on first page then our window will be [1] 2 3 4 5 6 7
+                break
+            elif i > preferred_end and len(page_range) == maxlen:
+                # if we have enough pages already then stop at preferred_end
+                break
+            page_range.append(i)
+
+        page_range = list(page_range)
+
+        if page_range[0] > 1:
+            page_range.insert(0, "...")
+
+        if page_range[-1] < total_pages:
+            page_range.append("...")
+
+        return page_range
+
     def get_context_data(self, **kwargs):
         context = super(TopicView, self).get_context_data(**kwargs)
         topic_id = self.kwargs['topic_id']
@@ -53,7 +91,10 @@ class TopicView(ForumBaseView):
         except EmptyPage:
             # If page is out of range (e.g. 9999), deliver last page of results.
             context['posts'] = posts_paginator.page(posts_paginator.num_pages)
+
         context["display_page_range"] = len(posts_paginator.page_range) > 1
+        context["page_range"] = self.get_page_range(int(page), posts_paginator.num_pages)
+
         context['topic'] = Topic.objects.get(pk=topic_id)
         context['post_form'] = PostForm()
 
