62e69f8c925ff9c3b26ff44ae6ca628db067f87b
Kai Diefenbach <kai.diefenbach@iqpp.de>
2015-06-19 11:21:13 +0200

    Adds hover effect for already filled in surveys (TREN-72).

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,3 +1,5 @@
+var TREN = TREN || {};
+
 $(function() {
     $(".needs-confirmation").on("click", function(e) {
         if (confirm("Möchtest Du wirklich alle Daten dieser Umfrage löschen?")) {
@@ -5,4 +7,15 @@ $(function() {
         };
         return false;
     });
+
+    // /my/umfragen
+    $(".survey-button a.done").on("mouseover", function(e) {
+        TREN.survey_button_title = $(this).html();
+        $(this).html("Bearbeiten");
+    });
+
+    $(".survey-button a.done").on("mouseout", function(e) {
+        $(this).html(TREN.survey_button_title);
+    });
+
 })
