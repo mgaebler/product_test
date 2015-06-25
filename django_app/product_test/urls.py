7cdd43e94aeff149d032d5589c75aea8628ec9b9
Kai Diefenbach <kai.diefenbach@iqpp.de>
2015-06-25 15:30:25 +0200

    Renames urls from surveys to umfragen.

--- django_app/product_test/urls.py
+++ django_app/product_test/urls.py
@@ -8,11 +8,10 @@ urlpatterns = patterns('',
 
     url(r'^(?P<slug>[\w-]+)/forum', include('product_test.urls_forum', namespace='forum')),
     url(r'^(?P<slug>[\w-]+)/galerie', include('product_test.urls_gallery', namespace='gallery')),
-    url(r'^(?P<slug>[\w-]+)/surveys', include('product_test.urls_surveys', namespace='surveys')),
+    url(r'^(?P<slug>[\w-]+)/umfragen', include('product_test.urls_surveys', namespace='surveys')),
     url(r'^(?P<slug>[\w-]+)/testergebnisse', views.ResultView.as_view(), name='result'),
 
     url(r'^(?P<slug>[\w-]+)/faq$', views.ProductTestDetail.as_view(template_name='product_test/faq.jinja'), name='faq'),
     url(r'^brands$', views.ProductBrandListView.as_view(), name='brand_list'),
     url(r'^brand/(?P<slug>[\w-]+)$', views.ProductBrandDetailView.as_view(), name='brand_detail'),
 )
-
