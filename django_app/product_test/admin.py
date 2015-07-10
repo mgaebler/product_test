06b3118973d6770e9e7d20332275bd619ca8558f
Kai Diefenbach <kai.diefenbach@iqpp.de>
2015-07-10 15:38:24 +0200

    Adds field to upload applications to a product test.

--- django_app/product_test/admin.py
+++ django_app/product_test/admin.py
@@ -1,9 +1,16 @@
 # coding: utf-8
 from __future__ import unicode_literals
+import csv
+import logging
+from django.conf.urls import patterns, url
 from django.contrib import admin
+from django.contrib import messages
+from django.shortcuts import redirect
 from django.templatetags.static import static
-
 from .models import ProductTest, Brand, Participation, TestResult
+from surveys.models import SurveyUser
+
+logger = logging.getLogger(__name__)
 
 
 class ParticipationInline(admin.TabularInline):
@@ -51,12 +58,62 @@ class ProductTestAdmin(admin.ModelAdmin):
         })
     )
 
-
     class Media:
         js = [
             static('grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js'),
             static('product_test/tinymce_setup/tinymce_setup.js'),
         ]
+
+    def upload_particpations(self, request, product_test_id):
+        """
+        View to add participation via upload a csv file.
+        """
+        try:
+            product_test = ProductTest.objects.get(pk=product_test_id)
+        except ProductTest.DoesNotExist:
+            messages.add_message(request, messages.ERROR, "Produkttest existiert nicht mehr!")
+            return redirect(request.META.get("HTTP_REFERER"))
+
+        participations = request.FILES.get("participations")
+        if not participations:
+            messages.add_message(request, messages.ERROR, "Datei ist erforderlich!")
+            return redirect(request.META.get("HTTP_REFERER"))
+
+        created = False
+        for row in csv.reader(participations, delimiter=str(';'), quotechar=str('"')):
+            try:
+                survey = product_test.application_survey
+                survey_user = SurveyUser.objects.get(
+                    survey=survey,
+                    uid=row[0],
+                )
+            except SurveyUser.DoesNotExist:
+                logger.info(u"User does not exist for survey '{}' and uid '{}'".format(survey.id, row[0]))
+            else:
+                created = True
+                Participation.objects.get_or_create(
+                    users=survey_user.user,
+                    product_test=product_test
+                )
+
+        if created:
+            messages.add_message(request, messages.INFO, "Participations wurden hinzugefügt.")
+        else:
+            messages.add_message(request, messages.WARNING, "Es wurden keine Participations hinzugefügt.")
+
+        return redirect(request.META.get("HTTP_REFERER"))
+
+    def get_urls(self):
+        """
+        Add the upload particpations view to urls.
+        """
+        urls = super(ProductTestAdmin, self).get_urls()
+        extra_urls = patterns("",
+            url("^(?P<product_test_id>\d+)/upload-particpations/$",
+                self.admin_site.admin_view(self.upload_particpations),
+                name="upload_particpations"),
+        )
+        return extra_urls + urls
 
 
 @admin.register(TestResult)
