532b82093b13446e279a912f5ac336260484528f
Kai Diefenbach <kai.diefenbach@iqpp.de>
2015-10-05 17:39:48 +0200

    TREN-120 Adds some german translations.

--- django_app/gallery/forms.py
+++ django_app/gallery/forms.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 
 class ImageUploadForm(forms.Form):
-    title = forms.CharField(required=False)
-    description = forms.CharField(widget=forms.Textarea, required=False)
-    image = forms.ImageField()
+    title = forms.CharField(label=_(u"Title"), required=False)
+    description = forms.CharField(label=_(u"Description"), widget=forms.Textarea, required=False)
+    image = forms.ImageField(label=_(u"Image"))
 
 
 class VideoLinkUploadForm(forms.Form):
