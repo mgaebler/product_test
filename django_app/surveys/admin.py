a6d068986cacd7a9ef1149be9252c8b1b97d593d
Kai Diefenbach <kai.diefenbach@iqpp.de>
2015-10-08 14:06:17 +0200

    Adds form to enrich survey with emails.

--- django_app/surveys/admin.py
+++ django_app/surveys/admin.py
@@ -1,4 +1,10 @@
+import csv
+import os
+from django.conf.urls import patterns, url
 from django.contrib import admin
+from django.contrib import messages
+from django.http import HttpResponse
+from django.shortcuts import redirect
 from . models import Survey
 from . models import SurveyUser
 
@@ -15,5 +21,43 @@ class SurveyUserInline(admin.TabularInline):
 
 class SurveyAdmin(admin.ModelAdmin):
     inlines = (SurveyUserInline,)
+
+    def add_email_addresses(self, request, survey_id):
+        """
+        Adds e-mail addresses to umfrage online file
+        """
+        umfrage_online_file = request.FILES.get("umfrage_online_file")
+        if not umfrage_online_file:
+            messages.add_message(request, messages.ERROR, "Datei Umfrage ist erforderlich!")
+            return redirect(request.META.get("HTTP_REFERER"))
+
+        filename = "{}-emails{}".format(*os.path.splitext(umfrage_online_file.name))
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename={}".format(filename)
+        writer = csv.writer(response, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+
+        for i, row in enumerate(csv.reader(umfrage_online_file, delimiter=';', quotechar='"')):
+            if i == 0:
+                row.insert(0, 'E-Mail')
+            else:
+                try:
+                    survey_user = SurveyUser.objects.get(survey_id=survey_id, uid=row[2])
+                except SurveyUser.DoesNotExist:
+                    row.insert(0, "User existiert nicht")
+                else:
+                    row.insert(0, survey_user.user.email)
+
+            writer.writerow(row)
+
+        return response
+
+    def get_urls(self):
+        urls = super(SurveyAdmin, self).get_urls()
+        extra_urls = patterns("",
+            url("^(?P<survey_id>\d+)/add-email-addresses/$",
+                self.admin_site.admin_view(self.add_email_addresses),
+                name="add_email_addresses"),
+        )
+        return extra_urls + urls
 
 admin.site.register(Survey, SurveyAdmin)
