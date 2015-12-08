04ff19a5e91b0af8dc07a2ffe90d7843a12b2c69
Marian Gaebler <marian.gaebler@gmail.com>
2015-12-08 14:57:42 +0100

    add more search and filter options for user accounts

--- django_app/user_accounts/admin.py
+++ django_app/user_accounts/admin.py
@@ -4,8 +4,9 @@ from .models import UserAccount
 
 class UserAccountAdmin(admin.ModelAdmin):
     raw_id_fields = ('invited_by',)
-    list_display = ('email', 'full_name', 'preferred_name', 'profile_complete',)
-    search_fields = ('email', 'full_name', 'preferred_name', 'profile_complete',)
+    list_display = ('email', 'full_name', 'preferred_name', 'profile_complete', 'created_at',)
+    list_filter = ('profile_complete', 'gender', 'family_status', 'is_staff', 'is_active',)
+    search_fields = ('email', 'full_name', 'preferred_name',)
 
 admin.site.register(UserAccount, UserAccountAdmin)
 
